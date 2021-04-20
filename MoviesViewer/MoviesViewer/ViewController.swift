e479439e48ea484c4b9e8693c6e0ad0f04a854ff
vishnumhn7 <36275673+vishnumhn7@users.noreply.github.com>
2021-04-20 11:17:54 +0530

    First Commit - Project Created

---
+++ MoviesViewer/MoviesViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesViewer
+//
+//  Created by Mohan, Vishnu on 20/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
